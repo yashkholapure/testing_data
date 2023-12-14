689d5987f6d39bad1fee6e27b4a94d6526e7a719
yashkholapure <96808894+yashkholapure@users.noreply.github.com>
2023-12-14 15:21:04 +0530

    Update2 update_pyfile.py

--- update_pyfile.py
+++ update_pyfile.py
@@ -1,36 +1,34 @@
-import os
 import requests
+import os
 
 # Define your Databricks details
 DATABRICKS_HOST = 'https://adb-5219522611471112.12.azuredatabricks.net'
 DATABRICKS_TOKEN = os.getenv('DATABRICKS_TOKEN')  # Fetch from GitHub repository secrets
+WORKSPACE_PATH = '/Workspace/Repos/git_checking/testing_data/testing'  # Path to your workspace directory
 
-def list_databricks_files(notebook_path):
+def list_files_in_workspace():
     url = f"{DATABRICKS_HOST}/api/2.0/workspace/list"
     headers = {
         'Authorization': f'Bearer {DATABRICKS_TOKEN}',
         'Content-Type': 'application/json'
     }
-    data = {
-        'path': notebook_path
-    }
+    data = {'path': WORKSPACE_PATH}
+
     try:
-        response = requests.post(url, headers=headers, json=data)
-        response.raise_for_status()
-        files = response.json()['objects']
-        file_names = [file['path'] for file in files if file['object_type'] == 'NOTEBOOK']
-        return file_names
+        response = requests.get(url, headers=headers, json=data)
+        response.raise_for_status()  # Raise an error for HTTP errors (status codes >= 400)
+        files_list = [file["path"] for file in response.json().get("objects", []) if file.get("object_type") == "NOTEBOOK"]
+        return files_list
     except requests.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
-        print(f"Detailed error message: {response.text}")
+        print(f"Detailed error message: {response.text}")  # Print detailed error message from the API
     except Exception as err:
         print(f"An error occurred: {err}")
     return None
 
 if __name__ == "__main__":
-    notebook_path = '/Workspace/Repos/git_checking/testing_data/testing'  # Change this to your desired path
-    files_list = list_databricks_files(notebook_path)
-    if files_list:
-        print("List of files in Databricks:")
-        for file_name in files_list:
+    file_names = list_files_in_workspace()
+    if file_names:
+        print("File names in the workspace:")
+        for file_name in file_names:
             print(file_name)
