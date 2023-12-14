e9ad01c6e74b7a6b050ef03ae3f6fc5d389d8961
yashkholapure <96808894+yashkholapure@users.noreply.github.com>
2023-12-14 15:48:07 +0530

    workspace

--- update_pyfile.py
+++ update_pyfile.py
@@ -4,21 +4,20 @@ import os
 # Define your Databricks details
 DATABRICKS_HOST = 'https://adb-5219522611471112.12.azuredatabricks.net'
 DATABRICKS_TOKEN = os.getenv('DATABRICKS_TOKEN')  # Fetch from GitHub repository secrets
-WORKSPACE_PATH = '/Workspace/Repos/git_checking/testing_data/testing'  # Path to your workspace directory
 
-def list_files_in_workspace():
+def list_items_in_workspace(path='/'):
     url = f"{DATABRICKS_HOST}/api/2.0/workspace/list"
     headers = {
         'Authorization': f'Bearer {DATABRICKS_TOKEN}',
         'Content-Type': 'application/json'
     }
-    data = {'path': WORKSPACE_PATH}
+    data = {'path': path}
 
     try:
         response = requests.get(url, headers=headers, json=data)
         response.raise_for_status()  # Raise an error for HTTP errors (status codes >= 400)
-        files_list = [file["path"] for file in response.json().get("objects", []) if file.get("object_type") == "NOTEBOOK"]
-        return files_list
+        items_list = [item["path"] for item in response.json().get("objects", [])]
+        return items_list
     except requests.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
         print(f"Detailed error message: {response.text}")  # Print detailed error message from the API
@@ -27,8 +26,8 @@ def list_files_in_workspace():
     return None
 
 if __name__ == "__main__":
-    file_names = list_files_in_workspace()
-    if file_names:
-        print("File names in the workspace:")
-        for file_name in file_names:
-            print(file_name)
+    workspace_items = list_items_in_workspace()
+    if workspace_items:
+        print("Items in the workspace:")
+        for item in workspace_items:
+            print(item)
