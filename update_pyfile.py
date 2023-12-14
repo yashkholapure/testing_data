cd5cf3932905b4645823edccfa29c4f795916e01
yashkholapure <96808894+yashkholapure@users.noreply.github.com>
2023-12-14 16:12:31 +0530

    Update update_pyfile.py

--- update_pyfile.py
+++ update_pyfile.py
@@ -1,33 +1,79 @@
-import requests
 import os
+import requests
+import base64
+
+print("Starting script execution...")
 
 # Define your Databricks details
 DATABRICKS_HOST = 'https://adb-5219522611471112.12.azuredatabricks.net'
 DATABRICKS_TOKEN = os.getenv('DATABRICKS_TOKEN')  # Fetch from GitHub repository secrets
 
-def list_items_in_workspace(path='/'):
-    url = f"{DATABRICKS_HOST}/api/2.0/workspace/list"
+# Path to your notebooks in Databricks workspace
+DATABRICKS_NOTEBOOK_PATH = '/Repos/git_checking/testing_data/testing'
+
+def delete_databricks_file(notebook_path):
+    url = f"{DATABRICKS_HOST}/api/2.0/workspace/delete"
     headers = {
         'Authorization': f'Bearer {DATABRICKS_TOKEN}',
         'Content-Type': 'application/json'
     }
-    data = {'path': path}
-
+    data = {
+        'path': notebook_path,
+        'recursive': False
+    }
     try:
-        response = requests.get(url, headers=headers, json=data)
-        response.raise_for_status()  # Raise an error for HTTP errors (status codes >= 400)
-        items_list = [item["path"] for item in response.json().get("objects", [])]
-        return items_list
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return response.status_code
     except requests.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
-        print(f"Detailed error message: {response.text}")  # Print detailed error message from the API
+        print(f"Detailed error message: {response.text}")
     except Exception as err:
         print(f"An error occurred: {err}")
     return None
 
+def update_databricks(notebook_name, notebook_content):
+    url = f"{DATABRICKS_HOST}/api/2.0/workspace/import"
+    headers = {
+        'Authorization': f'Bearer {DATABRICKS_TOKEN}',
+        'Content-Type': 'application/json'
+    }
+    notebook_content_base64 = base64.b64encode(notebook_content.encode('utf-8')).decode('utf-8')
+    data = {
+        'path': f"{DATABRICKS_NOTEBOOK_PATH}/{notebook_name}",
+        'content': notebook_content_base64,
+        'format': 'SOURCE',
+        'overwrite': 'false'
+    }
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return response.status_code
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        print(f"Detailed error message: {response.text}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
+    return None
+
+def delete_and_update_files():
+    # Delete files from Databricks
+    delete_status = delete_databricks_file(DATABRICKS_NOTEBOOK_PATH)
+    if delete_status == 200:
+        print(f"Deleted files from Databricks at path: {DATABRICKS_NOTEBOOK_PATH}")
+
+    # Fetch and upload files from GitHub to Databricks
+    github_files_path = os.getenv('GITHUB_WORKSPACE') + '/testing'
+    for root, dirs, files in os.walk(github_files_path):
+        for file in files:
+            if file.endswith('.py'):
+                notebook_name = file
+                notebook_content = open(os.path.join(root, file), 'r').read()
+                status_code = update_databricks(notebook_name, notebook_content)
+                if status_code == 200:
+                    print(f"Updated {notebook_name} in Databricks.")
+                else:
+                    print(f"Failed to update {notebook_name} in Databricks. Status code: {status_code}")
+
 if __name__ == "__main__":
-    testing_data_items = list_items_in_workspace('/Repos/git_checking/testing_data/testing')
-    if testing_data_items:
-        print("Items in the '/Repos/git_checking/testing_data/testing' directory:")
-        for item in testing_data_items:
-            print(item)
+    delete_and_update_files()
