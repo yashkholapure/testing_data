f49acdeb2f8d162dc623239fda4789ba60bd5c13
yashkholapure <96808894+yashkholapure@users.noreply.github.com>
2023-12-14 13:39:10 +0530

    Update3 update_pyfile.py

--- update_pyfile.py
+++ update_pyfile.py
@@ -9,7 +9,7 @@ DATABRICKS_HOST = 'https://adb-5219522611471112.12.azuredatabricks.net'
 DATABRICKS_TOKEN = os.getenv('DATABRICKS_TOKEN')  # Fetch from GitHub repository secrets
 
 # Path to your notebooks in Databricks workspace
-DATABRICKS_NOTEBOOK_PATH = '/Workspace/Repos/git_checking/testing_data/'
+DATABRICKS_NOTEBOOK_PATH = '/Workspace/Repos/git_checking/testing_data/testing'
 
 def update_databricks(notebook_name, notebook_content):
     url = f"{DATABRICKS_HOST}/api/2.0/workspace/import"
